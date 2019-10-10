3afb1139d46be9cc9e10ad152935196b8b9c8622
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-10-10 20:28:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,4 +14,27 @@ class TicTacToe
     [2, 4, 6]
   ]
 
+  def display_board
+      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+      puts "-----------"
+      puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+      puts "-----------"
+      puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(user_input)
+   user_input.to_i - 1
+  end
+
+  def move(position, char)
+   @board[position] = char
+  end
+
+  def position_taken?(index_i)
+   ((@board[index_i] == "X") || (@board[index_i] == "O"))
+  end
+
+  def valid_move?(index)
+   index.between?(0,8) && !position_taken?(index)
+  end
 end
