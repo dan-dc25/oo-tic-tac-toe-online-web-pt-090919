871494eab27e18dcd2b838ce85a7ece3842cf5a4
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-10-10 21:18:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -112,5 +112,15 @@ class TicTacToe
     end
   end
 
+  def play
+    while over? == false
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
 
 end
